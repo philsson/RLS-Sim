b4e50c9032199b9065ba74fd07d20320216a70d7
johan9107 <jgr12001@student.mdh.se>
2017-02-15 14:58:43 +0100

    Improved RLS

--- RLS_FUNC.m
+++ RLS_FUNC.m
@@ -1,44 +1,51 @@
-function [ RlsOut, weightsOut, errorOut] = RLS_FUNC( u, y, complexity)
+function [ rls_data] = RLS_FUNC(y, u, rls_data)
 
-% complexity = should be even numbers. Always start with 2
+%-- rls_data. --
+
+%complexity = Matrix dimensions, should be even numbers. Always start with 2
+%RlsOut     = output value of rls approximation funciton
+%weights    = vector, deriving tuning rules
+%fi         = vector with previous in and outputs [y(k-1 u(k-1)]'
+%error      = output error of RLS
+%V          = matrix
+%K          = matrix
+
+%NOTE: You only need to run the function, NOT assign any values to rls_data
+%      However, remeber to assign rls_data to its inpus as well as its
+%      output when you use it: rls_data = RLS_FUNC(y, u, rls_data)
 
 % --- Init values ---
 
-persistent weights error;
-persistent fi K V;
-
-if isempty(weights)
+if isempty(rls_data.weights) || isempty(rls_data.fi)
     
-    weights = rand(1,complexity)';
-    V = rand(complexity,complexity);
-    fi = rand(1,complexity)';     
-    K = rand(1,complexity)';  
-    error = 0; 
+    rls_data.weights = rand(1,rls_data.complexity)';
+    rls_data.V = rand(complexity,rls_data.complexity);
+    rls_data.fi = rand(1,rls_data.complexity)';     
+    rls_data.K = rand(1,rls_data.complexity)';  
+    rls_data.error = 0; 
     
 end
 
 % -- RLS  --- 
 
-b = 1 + fi'*V*fi;
-weights = weights + K*error;
-K = V*fi;
-error = y - fi'*weights;
-V = V - inv(b)*V*fi*fi'*V;
+b = 1 + rls_data.fi'*rls_data.V*rls_data.fi;
+rls_data.weights = rls_data.weights + rls_data.K*rls_data.error;
+rls_data.K = rls_data.V*rls_data.fi;
+rls_data.error = rls_data.y - rls_data.fi'*rls_data.weights;
+rls_data.V = rls_data.V - inv(b)*rls_data.V*rls_data.fi*rls_data.fi'*rls_data.V;
 
 %-- Update fi values ---
-y_in = circshift(fi(1:(length(fi))/2),1)';
-u_in = circshift(fi(((length(fi))/2) + 1:end),1)';
+y_in = circshift(rls_data.fi(1:(length(rls_data.fi))/2),1)';
+u_in = circshift(rls_data.fi(((length(rls_data.fi))/2) + 1:end),1)';
 
 y_in(1) = y;
 u_in(1) = u;
 
-fi = [y_in u_in]';
+rls_data.fi = [y_in u_in]';
 
 %-- Update outputs ---
 
-RlsOut = fi'*weights;
-weightsOut = weights;
-errorOut = error;
+rls_data.RlsOut = fi'*weights;
 
 end
 
