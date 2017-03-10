dfbd1b9c9d93dcb537eea88deb17dd22de3e58d6
johan9107 <johan@smiley-air.mdh.se>
2017-03-10 16:02:14 +0100

    Added bug fix to plot

--- plot_saved_data_for_log.m
+++ plot_saved_data_for_log.m
@@ -14,7 +14,7 @@ if logs_enabled(3) == true
     stairs(1:length(ki_data_z_axis), ki_data_z_axis);
     stairs(1:length(kd_data_z_axis), kd_data_z_axis);
     
-    axis([0 loop_counter -(max(kp_data_z_axis)) (max(kp_data_z_axis)+max(kp_data_z_axis)*0.1)]);
+    axis([0 loop_counter -(min(kp_data_z_axis)*1.1) (max(kp_data_z_axis)+max(kp_data_z_axis)*1.1)]);
     ylabel('Pid values');
     legend('Kp', 'Ki', 'Kd')
     title('Z axis - PID values and FOPDT');
@@ -53,7 +53,7 @@ if logs_enabled(3) == true
     stairs(1:loop_counter-1, weights_data_z_axis(1,:));
     stairs(1:loop_counter-1, weights_data_z_axis(2,:));
     
-    axis([0 loop_counter min(weights_data_z_axis(2,loop_counter*0.01:end)) max(weights_data_z_axis(2,loop_counter*0.01:end))]);
+    axis([0 loop_counter (min(weights_data_z_axis(1,loop_counter*0.01:end))+0.1) max(weights_data_z_axis(2,loop_counter*0.01:end))*1.1]);
     ylabel('Weights output');
     legend('a1', 'b1');
            
