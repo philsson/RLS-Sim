7529814dca85399c137481fbe2f60c0dc421418c
johan9107 <jgr12001@student.mdh.se>
2017-04-24 09:27:08 +0200

    added smootch plots

--- Plot functions/print_smooth.m
+++ Plot functions/print_smooth.m
@@ -26,13 +26,54 @@ grid on
 plot(path(1,:),path(2,:))
 xlabel('x-pos'); ylabel('y-pos');
 subplot(2,2,2)
+
+x=-4:.1:4;
+[X,Y] = meshgrid(x);
+Z= (X*Y) - 500 + 58.1; %meshgrid(0:0.1:1);
+
 hold on
 grid on
-plot3(path(1,1:step:end),path(2,1:step:end),path(3,1:step:end),'k')
-stem3(path(1,1:step:end),path(2,1:step:end),path(3,1:step:end)')
+plot3(path(1,1:step:end),path(2,1:step:end),path(3,1:step:end),'LineWidth',4)
+plot3(path(1,1:step:end/4),path(2,1:step:end/4),path(3,1:step:end/4),'LineWidth',0.6, 'Color', 'y')
+plot3(path(1,end/4:step:end/2),path(2,end/4:step:end/2),path(3,end/4:step:end/2),'LineWidth',0.6, 'Color', 'g')
+plot3(path(1,end/2:step:end/1),path(2,end/2:step:end/1),path(3,end/2:step:end/1),'LineWidth',0.6, 'Color', 'g')
+% stem3(path(1,1:step:end),path(2,1:step:end),path(3,1:step:end)','Color', 'y')
+% plot3(path(1,1:step:end),path(2,1:step:end),path(3,1:step:end),'o', 'Color', 'r')
+
+le = 1.5;
+la = 1.4
+
+le2 = 2.1;
+la2 = 1.8;
+
+le3 = 22
+la3 = 13;
+
+le4 = 3.5
+la4 = 3.4;
+
+
+%plot3(path(1,end/6:step:end),path(2,end/6:step:end),path(3,end/6:step:end),'LineWidth',3)
+stem3(path(1,end/le:step:end/la),path(2,end/le:step:end/la),path(3,end/le:step:end/la)','Color', 'r')
+stem3(path(1,end/le2:step:end/la2),path(2,end/le2:step:end/la2),path(3,end/le2:step:end/la2)','Color', 'r')
+stem3(path(1,end/le3:step:end/la3),path(2,end/le3:step:end/la3),path(3,end/le3:step:end/la3)','Color', 'k')
+stem3(path(1,end/le4:step:end/la4),path(2,end/le4:step:end/la4),path(3,end/le4:step:end/la4)','Color', 'm')
+%plot3(path(1,end/6:step:end),path(2,end/6:step:end),path(3,end/6:step:end),'o', 'Color', 'r')
+
+%plot3(path(1,1:step:end),path(2,1:step:end),path(3,1:step:end),'LineWidth',3 ,'Color', 'r')
+%stem3(path(1,1:step:end),path(2,1:step:end),path(3,1:step:end)','Color', 'y')
+
+%surf(X,Y,Z)
+colormap([1, 0, 0
+    1, 0, 0
+    1, 0, 0
+    1, 0, 0
+    1, 0, 0])
+
 xlabel('x-pos'); ylabel('y-pos'); zlabel('elevation');
 view([240 23])
 subplot(2,2,[3 4]) 
+
 hold on
 grid on
 plot(time, path(1,:),time, path(2,:), time, path(3,:))
